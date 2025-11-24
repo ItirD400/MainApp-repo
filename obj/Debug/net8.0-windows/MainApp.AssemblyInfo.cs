ad58ce60a707d8419f7c31326ba4063e52a0efb1
Jackson Huang <JacksonHuang@itri.org.tw>
2025-11-24 14:45:11 +0800

    Added submodules

--- obj/Debug/net8.0-windows/MainApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MainApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18aa9f72455e1fef84596de2fb388425f0637510")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
